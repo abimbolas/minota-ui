b66bae32b8e464615ce5021555de9dfd62ece073
Vitaly Kuleshin <antivitla@gmail.com>
2018-11-04 22:29:56 +0300

    config

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,38 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <div>
+        <router-link to="/">Home</router-link> |
+        <!-- <router-link to="/about">About</router-link> -->
+        <router-link to="/config">Config</router-link>
+      </div>
+
+      <div><small>Dropbox/Notes</small></div>
+
     </div>
     <router-view/>
   </div>
 </template>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-}
+<style lang="stylus">
+body
+  padding 3em
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
+#app
+  font-family Helvetica, Arial, sans-serif
+  -webkit-font-smoothing antialiased
+  -moz-osx-font-smoothing grayscale
+  color #2c3e50
 
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
+#nav
+  display flex
+  justify-content space-between
+
+#nav a
+  font-weight bold
+  color #2c3e50
+
+#nav a.router-link-exact-active
+  color #42b983
+
 </style>
---
+++ src/config/config.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="config">
+    <h1>Settings</h1>
+    <h4>Storage</h4>
+    <dl>
+      <dt>Use third-party sync</dt>
+      <small>Use installed Dropbox, Google Drive, OneDrive or Yandex Disk as storage. Write path where you want to store your data, e.g. 'c:/Users/antivitla/Dropbox/Notes' (win) or '/Users/antivitla/Dropbox/Notes' (osx).</small>
+      <dd>(Dropbox, Google Drive, OneDrive, YandexDisk)</dd>
+
+      <br>
+      <br>
+
+      <dt>Use Mitaba account</dt>
+      <small>Login with Facebook, Google, Github, VK or Yandex into your Mitaba/Titamota account</small>
+      <dd>(login with social auth)</dd>
+
+      <br>
+      <br>
+
+      <dt>Other url</dt>
+      <small>If you want to use your own standalone minota server, provide it's url here</small>
+      <dd>(url)</dd>
+    </dl>
+  </div>
+</template>

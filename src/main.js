302adc80170a726ae200c0dca1df58b48de70c1d
Виталий Кулешин <antivitla@gmail.com>
2020-07-26 00:20:57 +0300

    remove unneeded

--- src/main.js
+++ src/main.js
@@ -2,13 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import filters from './filters'
+// import filters from './filters'
 // import '@/Material'
 // import './registerServiceWorker';
 
-Object.keys(filters).forEach(key => {
-  Vue.filter(key, filters[key])
-})
+// Object.keys(filters).forEach(key => {
+//   Vue.filter(key, filters[key])
+// })
 
 Vue.config.productionTip = false
 
